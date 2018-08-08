d3eefab35e0a9e653e340707cd9a4875889a064f
Jonathan Caley <jonathancaley@hotmail.co.uk>
2018-08-08 21:58:26 +0100

    Tile descriptions re-done
    Tile descriptions re-done with padding and links to GitHub repositoried
    
    Parallax image now responds to the position of the mouse

--- scripts/master.js
+++ scripts/master.js
@@ -49,7 +49,11 @@ output.addEventListener("scroll", function() {
     }
 });
 
+var state = 0;
+
 function showFixedContainer() {
+
+    console.log("showing");
     fidexLinkContainer.style.visibility = "visible";
 
     $("#fixed-container-experience").addClass("animated fadeInRight");
@@ -63,9 +67,15 @@ function showFixedContainer() {
     $("#fixed-container-github").removeClass("fadeOutRight");
     $("#fixed-container-hotmail").removeClass("fadeOutRight");
     $("#fixed-container-linkedin").removeClass("fadeOutRight");
+
+    state = 1;
+
 }
 
 function hideFixedContainer() {
+
+    state = 0;
+    console.log("hiding");
     $("#fixed-container-experience").removeClass("fadeInRight");
     $("#fixed-container-education").removeClass("fadeInRight");
     $("#fixed-container-github").removeClass("fadeInRight");
@@ -78,6 +88,19 @@ function hideFixedContainer() {
     $("#fixed-container-hotmail").addClass("fadeOutRight");
     $("#fixed-container-linkedin").addClass("fadeOutRight");
 
+
+    stateChange()
+    // fidexLinkContainer.style.visibility = "collapse";
+
+    // fidexLinkContainer.style.zIndex = "0";
+}
+
+function stateChange() { //hides the fixed layout to prevent overlapping of links - needs the delay to allow the animation then checks if the state is still valid for it to collapse after the time
+    setTimeout(function(){
+        if(state == 0){
+            fidexLinkContainer.style.visibility = "collapse";
+        }
+    }, 1000);
 }
 
 function scrollToView(view) {
@@ -94,3 +117,41 @@ function isScrolledIntoView(elem)
 
     return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
 }
+
+
+
+
+
+
+
+var lFollowX = 0,
+    lFollowY = 0,
+    x = 0,
+    y = 0,
+    friction = 1 / 30;
+
+function moveBackground() {
+  x += 3*(lFollowX - x) * friction;
+  y += 3*(lFollowY - y) * friction;
+
+  translate = 'translate(' + x + 'px, ' + y + 'px) translateZ(-1px) scale(2.1)';
+
+  $('.ParallaxContainer').css({
+    '-webit-transform': translate,
+    '-moz-transform': translate,
+    'transform': translate
+  });
+
+  window.requestAnimationFrame(moveBackground);
+}
+
+$(window).on('mousemove click', function(e) {
+
+  var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+  var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+  lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
+  lFollowY = (10 * lMouseY) / 100;
+
+});
+
+moveBackground();
